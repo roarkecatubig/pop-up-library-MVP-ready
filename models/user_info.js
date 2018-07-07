d7cd0b34d6d2c41dbb4a0bee0d8681fce60964d9
unknown <caylenepicache@gmail.com>
2018-07-07 13:18:53 -0700

    allow null schema change for pass/zip/rating

--- models/user_info.js
+++ models/user_info.js
@@ -1,53 +1,54 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-  id: {
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.INTEGER
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        len: [1,18]
-    }
-  },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  email: {
+    var User = sequelize.define("User", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-zipCode: {
-    type: DataTypes.INTEGER, 
-    allowNull: false
-  },
-rating: {
-    type: DataTypes.INTEGER, 
-    allowNull: false
-  },
-createdAt: {
-    allowNull: false,
-    type: DataTypes.DATE
-  },
-last_login: {
-    type: DataTypes.DATE
-  }
+      validate: {
+          len: [1,18]
+      }
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+  zipCode: {
+      type: DataTypes.INTEGER, 
+      allowNull: true
+    },
+  rating: {
+      type: DataTypes.INTEGER, 
+      allowNull: true
+    },
+  createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+  last_login: {
+      type: DataTypes.DATE
+    }
 
-  });
-
-  User.associate = function(models) {
-    
-    User.hasMany(models.Book, {
-      onDelete: "cascade"
     });
+  
+    User.associate = function(models) {
+      
+      User.hasMany(models.Book, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
   };
-
-  return User;
-};
+  
